071688663199506257806ed97dfa2d4e9c0ff16d
nahlayajf <06639425+nahlayajf@users.noreply.github.com.>
2026-07-15 12:32:01 -0400

    WIP Create a grid of squares

--- script.js
+++ script.js
@@ -3,32 +3,24 @@ function promptSize() {
     let size = prompt("Please enter number 1-100:", "16")
     return size;
 }
-const square = document.createElement("div");
-square.classList.add("square");
-container.appendChild(square);
 
 
 
+function genGrid(size) {
+    let n = size;
+    totalSquares = (n * (n + 1) * (2 * n + 1)) / 6
+    for (let i = 0; i < totalSquares - 1; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
 
+    }
 
+    // console.log(totalSquares) fix how many squares are being genrated
+}
 
-
-// function genGrid(size) {
-//     let n = size;
-//     totalSquares = (n * (n + 1) * (2 * n + 1)) / 6
-//     // fix n 
-//     for (let i = 0; i < totalSquares; i++) {
-//         const square = document.createElement("div");
-//         square.classList.add("square");
-//         container.appendChild(square);
-//         square.style.borderColor = "black";
-//         square.style.borderWidth = "3px";
-
-//     }
-// }
-
-// let size = promptSize();
-// genGrid(size);
+let size = promptSize();
+genGrid(size);
 
 
 // When User clicks "new grid button"
